7f0ca893d0bd9ff22b0bda9a6f5775d56b33c22e
Noga <nogaran01@gmail.com>
2022-06-29 13:58:03 +0300

    start add note cmp

---
+++ js/apps/keep/cmps/add-note.cmp.js
@@ -0,0 +1,35 @@
+export default {
+    template: `
+    <div class="keep-app-add-note" @click="NoteEdit">
+        <input v-if="!isToDo" type="text" v-model="userInput" v-bind:placeholder="placeHolderMsg">
+        <textarea v-else v-bind:placeholder="placeHolderMsg" type="text" v-model="userInput"></textarea>
+        <button @click="isNote = true, isImage=false,isVideo=false,isToDo=false,placeHolderMsg='enter a new note'" :class="{'keep-button-choice': isNote}">N</button>
+        <button @click="isNote = false, isImage=true,isVideo=false,isToDo=false,placeHolderMsg='enter img url'" :class="{'keep-button-choice': isImage}">I</button>
+        <button @click="isNote = false, isImage=false,isVideo=true,isToDo=false,placeHolderMsg='enter video url'" :class="{'keep-button-choice': isVideo}">Y</button>
+        <button @click="isNote = false, isImage=false,isVideo=false,isToDo=true,placeHolderMsg='enter new todo list'" :class="{'keep-button-choice': isToDo}">TD</button>
+    </div>
+  `
+  ,components:{
+  },
+    data() {
+      return {
+            userInput: null,
+            isNote:true,
+            isImage:null,
+            isVideo:null,
+            isToDo:null,
+            placeHolderMsg: 'enter a new note'
+
+        }
+    },
+    methods: {
+        NoteEdit(){
+            if(!this.userInput) return
+            console.log(this.userInput, this.isNote, this.isImage, this.isVideo,this.isToDo);
+        }
+    },
+    created(){
+    },
+    computed: {},
+  };
+  
--- js/apps/keep/pages/keep-mainpage.js
+++ js/apps/keep/pages/keep-mainpage.js
@@ -1,5 +1,13 @@
+import addNote from '../cmps/add-note.cmp.js'
+
 export default {
     template: `
-    <h1>Hello</h1>
+    <div class="keep-app-main-page">
+        <h1>google keep</h1>
+        <add-note/>
+    </div>
     `,
+    components:{
+        addNote,
+    },
 }
