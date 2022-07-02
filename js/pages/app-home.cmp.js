4a99ec256822b1bf977602345562dd08d9278781
Noga <nogaran01@gmail.com>
2022-07-02 10:06:09 +0300

    homepage design

--- js/pages/app-home.cmp.js
+++ js/pages/app-home.cmp.js
@@ -1,12 +1,29 @@
+
 export default {
     template: `
     <section class="home-page">
-      <div class="headings">
-        <h1>Welcome to AppSus</h1>
-        <!-- <img src="img/home-page.jpg" class=img-homepage alt=""> -->
-        <button @click="goToAbout" class="start">about</button>
-        <button @click="goToKeep" class="start">keep</button>
-        <button @click="goToMail" class="start">mail</button>
+      <div class="home-desc">
+        <h1>Meet Appsus</h1>
+        <p>This new and game-changer app is Cupcake ipsum dolor sit amet wafer brownie cupcake jelly. Dragée jujubes tiramisu gummi bears cotton candy tiramisu pudding. Caramels donut donut halvah tiramisu bonbon tiramisu donut.</p>
+
+        <img src="../../images/main-page/devices.png" alt="devices">
+      </div>
+
+        <h1>Shall We?</h1>
+        <div class="home-links">
+          <span @click="goToAbout" class="start">
+            <p>About</p>
+            <img src="../../images/main-page/people-group-solid.svg" alt="">
+          </span>
+          <span class="link-miss-keep" @click="goToKeep" class="start">
+            <p>Miss Keep</p>
+            <img src="../../images/main-page/map-pin-solid.svg" alt="">
+          </span>
+          <span @click="goToMail" class="start">
+            <p>Mail</p>
+            <img src="../../images/main-page/envelope-solid.svg" alt="">
+          </span>
+        </div>
       </div>
    </section>
   `,
