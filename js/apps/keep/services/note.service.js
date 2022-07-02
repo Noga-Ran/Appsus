b4ff9cb14e6dc8aa3404f9c919d33af55d4abcd2
Noga <nogaran01@gmail.com>
2022-07-02 18:34:47 +0300

    photo

--- js/apps/keep/services/note.service.js
+++ js/apps/keep/services/note.service.js
@@ -87,7 +87,7 @@ function getNotes(){
             type: "note-img",
             isPinned: false,
             info: {
-                url: "https://images.unsplash.com/photo-1548199973-03cce0bbc87b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+                url: "https://freepngimg.com/thumb/dog/6-dog-png-image.png",
                 title: "Bobi and Me"
             },
             style: {
